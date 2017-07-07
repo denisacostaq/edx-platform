81cc6f145e979f0dea7bc2b77afe37c2becc9255
Harry Rein <hrein@edx.org>
2017-07-07 16:18:00 -0400

    Fixing small naming issue and updating code for readability.

--- openedx/features/course_experience/static/course_experience/js/CoursetalkReviews.js
+++ openedx/features/course_experience/static/course_experience/js/CoursetalkReviews.js
@@ -5,23 +5,28 @@
 export class CourseTalkReviews {  // eslint-disable-line import/prefer-default-export
   constructor() {
     const $courseTalkToggleReadWriteReviews = $('.toggle-read-write-reviews');
-    const $courseTalkScriptObject = $('.coursetalk-reviews-script');
 
     const readSrc = '//d3q6qq2zt8nhwv.cloudfront.net/s/js/widgets/coursetalk-read-reviews.js';
     const writeSrc = '//d3q6qq2zt8nhwv.cloudfront.net/s/js/widgets/coursetalk-write-reviews.js';
+
+    const toReadBtnText = 'View Reviews';
+    const toWriteBtnText = 'Write a Review';
+
+    // Initialize page to the read reviews view
+    var currentSrc = readSrc;
+    $courseTalkToggleReadWriteReviews.text(writeBtnText);
 
     $coursetalkToggleReadWriteReviews.on('click', () => {
       // Cache js file for future button clicks
       $.ajaxSetup({ cache: true });
 
       // Toggle the new coursetalk script object
-      const currentSrc = $courseTalkScriptObject.attr('src');
-      const newSrc = (currentSrc === readSrc) ? writeSrc : readSrc;
-      $courseTalkScriptObject.attr('src', newSrc);
-      $.getScript(newSrc);
+      const switchToReadView = currentSrc === writeSrc;
+      const newSource = switchToReadView ? readSrc : writeSrc;
+      $.getScript(newSource);
 
-      // Switch the button text
-      const newText = (newSrc === readSrc) ? 'Write a Review' : 'View Reviews';
+      // Toggle button text on switch to the other view
+      const newText = switchToReadView ? toWriteBtnText : toReadBtnText;
       $courseTalkToggleReadWriteReviews.text(newText);
     });
   }
